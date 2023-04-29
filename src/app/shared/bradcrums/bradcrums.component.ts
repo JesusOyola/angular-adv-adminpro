37740b1a2d3af6d008d9b56b147d311208ec324a
JesusOyola <jesus.oyola@interatica.com>
2023-04-29 16:48:31 -0300

    fin seccion 3 diseño

---
+++ src/app/shared/bradcrums/bradcrums.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bradcrums',
+  templateUrl: './bradcrums.component.html',
+})
+export class BradcrumsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
