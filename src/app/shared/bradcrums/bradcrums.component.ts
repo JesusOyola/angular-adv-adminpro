c806139f834f824393e14706720a392284ba7aad
JesusOyola <jesus.oyola@interatica.com>
2023-05-13 15:06:07 -0300

    fin de la seccion 8

--- src/app/shared/bradcrums/bradcrums.component.ts
+++ src/app/shared/bradcrums/bradcrums.component.ts
@@ -1,14 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-bradcrums',
   templateUrl: './bradcrums.component.html',
 })
-export class BradcrumsComponent implements OnInit {
+export class BradcrumsComponent implements OnDestroy {
+  titulo?: string;
+  tituloSubs$?: Subscription;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router) {
+    this.tituloSubs$ = this.getArgumentosRuta().subscribe((data) => {
+      this.titulo = data['titulo'];
+      document.title = `AdminPro - ${this.titulo}`;
+    });
+  }
+  ngOnDestroy(): void {
+    this.tituloSubs$?.unsubscribe();
   }
 
+  getArgumentosRuta() {
+    return this.router.events.pipe(
+      filter((event: any) => event instanceof ActivationEnd),
+      filter((event: ActivationEnd) => event.snapshot.firstChild === null),
+      map((event: ActivationEnd) => event.snapshot.data)
+    );
+  }
 }
